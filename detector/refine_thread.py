3c592a56f7d2c185e585ab98cc4a4c464bcb952a
wjh_job <2450525294@qq.com>
2026-06-03 13:59:27 +0800

    修复 refine_thread: 1.解决每帧重新创建和销毁线程池问题 2.确保输出给下游的refine_pts 维度符合num_keypoints(=7) 3.检测不完全的情况，不再使用 原始keypoints代替

--- detector/refine_thread.py
+++ detector/refine_thread.py
@@ -3,176 +3,196 @@ import time
 import numpy as np
 import queue
 from concurrent.futures import ThreadPoolExecutor
+from dataclasses import dataclass, field
+from core.queues import put_latest
 from core.logger import setup_logger
+from core.packet import FramePacket
+from config.queue_config import QueueConfig
 from detector.refine_ellipses import *
 
+@dataclass
+class RefineThreadConfig:
+    max_workers: int = 4
+    num_keypoints: int =7
+    queue_config: QueueConfig = field(
+        default_factory=QueueConfig)
+
 class RefineThread(threading.Thread):
-    def __init__(self, in_q, out_q, stop_event, cfg):
+    def __init__(self, in_q:queue.Queue, out_q:queue.Queue,
+                 stop_event:threading.Event, cfg:RefineThreadConfig):
         super().__init__(name="RefineThread", daemon=True)
         self.in_q = in_q
         self.out_q = out_q
         self.stop_event = stop_event
         self.cfg = cfg
         self.logger = setup_logger("RefineThread")
-        if self.in_q is None or self.out_q is None:
-            self.logger.error(
-                "Input or output queue is None, Thread will exit")
-            self.stop_event.set()
+        self.executor = ThreadPoolExecutor(
+            max_workers=self.cfg.max_workers
+        )
 
-    def extract_sub_roi(self, image, keypoints)-> tuple:
+    def _extract_sub_roi(self, image, keypoints)-> tuple:
         """Extract sub-ROI images ans top-left corners 
         around each keypoint based on the large and small hole widths.
         """
-        if image is None:
-            self.logger.warning(
-                "No image for sub-ROI extraction")
-            return None, None
-        if keypoints is None or len(keypoints) != 7:
-            self.logger.warning(
-                "Invalid keypoints for sub-ROI extraction")
-            return None, None
-
         # get large and small hole widths
         rect_s = np.linalg.norm(keypoints[1]-keypoints[0])
         rect_l = np.linalg.norm(keypoints[6]-keypoints[5])
 
-        # extract sub-ROIs around each keypoint
         sub_roi_imgs = []
         tls = [] # top-left corners
-        for i, keypoint in enumerate(keypoints):
-            wh = rect_l if i in [0,1] else rect_s
+        h, w = image.shape[:2]
+        # extract sub-ROIs around each keypoint
+        for i, kp in enumerate(keypoints):
+            width = rect_l if i in (0, 1) else rect_s
             # top-left and bottom-right corners of the sub-ROI
-            tl = (int(keypoint[0]-wh/2), int(keypoint[1]-wh/2))
-            br = (int(keypoint[0]+wh/2), int(keypoint[1]+wh/2))
-            sub_roi_img = image[tl[1]:br[1], tl[0]:br[0]]
-            sub_roi_imgs.append(np.ascontiguousarray(sub_roi_img))
-            tls.append(tl)
-        tls = np.asanyarray(tls)  # shape (7,2)
-        return sub_roi_imgs, tls
+            x1 = int(kp[0] - width / 2)
+            y1 = int(kp[1] - width / 2)
 
+            x2 = int(kp[0] + width / 2)
+            y2 = int(kp[1] + width / 2)
+
+            # avoid out of boundary
+            x1_clip = max(0, x1)
+            y1_clip = max(0, y1)
+
+            x2_clip = min(w, x2)
+            y2_clip = min(h, y2)
+
+            if x2_clip <= x1_clip or y2_clip <= y1_clip:
+                raise RuntimeError(
+                    f"Invalid ROI for kp[{i}] "
+                    f"kp={kp}, "
+                    f"roi=({x1},{y1})-({x2},{y2}), "
+                    f"image={image.shape}"
+                )
+
+            roi = image[
+                y1_clip:y2_clip,
+                x1_clip:x2_clip
+            ]
+
+            sub_roi_imgs.append(
+                np.ascontiguousarray(roi)
+            )
+
+            tls.append(
+                (x1_clip, y1_clip)
+            )
+
+        return sub_roi_imgs, np.asarray(tls)
     
-    def refine_point(self, image, keypoints):
+    def _refine_point(self, image, keypoints):
         """Refine keypoints by fitting ellipses to the sub-ROIs and 
         finding the best fit points. This is a placeholder implementation.
         """
-        if image is None or keypoints is None:
-            self.logger.warning(
-                "No image or keypoints for refinement")
-            return None
-
-        sub_roi_imgs, tls = self.extract_sub_roi(image, keypoints)
-        if sub_roi_imgs is None:
-            self.logger.warning(
+        try:
+            sub_roi_imgs, tls = self._extract_sub_roi(image, keypoints)
+        except Exception:
+            self.logger.error(
                 "Failed to extract sub-ROIs for refinement")
-            return None
-        max_workers = self.cfg.get('refiner', {}).get('max_workers', 8)
-        with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            result = executor.map(detect_and_refine_ellipses, sub_roi_imgs)
-
-        # convert to original image coordinates
-        # filter out None results from failed detections
-        valid_results = [res for res in result if res is not None]
+            raise
+        t0 = time.perf_counter_ns()
+        results = list(self.executor.map(
+            detect_and_refine_ellipses, sub_roi_imgs))
+        self.logger.info(
+            f"threadpool exec detect_and_refine_ellipses time: \
+                {(time.perf_counter_ns() - t0) / 1e6:.2f}ms")    
+        refined_pts = []
+        for i, res in enumerate(results):
+            if res is not None:
+                # convert to original image coordinates
+                refined_pts.append(res['p']+tls[i])
         
-        if len(valid_results) == 0:
-            self.logger.warning(
-                "All ellipse detections failed, no valid results")
-            return None
-            
-        if len(valid_results) != len(sub_roi_imgs):
-            self.logger.warning(
-                f"Partial detection failure: {len(valid_results)}/{len(sub_roi_imgs)} points detected")
-        
-        refined_ellipses = np.array([res['p'] for res in valid_results])  # shape (N,2)
+        return np.asarray(refined_pts)
 
-        # check dimension consistency
-        if refined_ellipses.shape[0] != tls.shape[0]:
-            self.logger.warning(
-                "Refined ellipses and top-left corners have inconsistent dimensions" \
-                "Refined ellipses shape: {}, top-left corners shape: {}".format(
-                    refined_ellipses.shape, tls.shape))
-            return None
-        
-        refined_pts = refined_ellipses + tls  # dims should match
-        return refined_pts
-
-    def process(self, packet):
+    def process(self, packet:FramePacket):
         t0 = time.time()
-        if not self.validate_packet(packet):
-            self.logger.warning("Invalid packet for refinement")
-            return None
-        refined_pts = self.refine_point(
-            packet.image, packet.keypoints)
-        if refined_pts is None:
+        try:
+            self._validate_packet(packet)
+        except Exception:
             self.logger.warning(
-                "Refinement failed, returning original keypoints")
-            refined_pts = packet.keypoints
+                f"Invalid packet for refinement")
+            raise
+        
+        try:
+            refined_pts = self._refine_point(
+                packet.image, packet.keypoints)
+        except Exception:
+            self.logger.warning("Refinement failed")
+            raise
+
+        # check dims
+        if refined_pts.shape != (self.cfg.num_keypoints,2):
+            self.logger.warning(
+                f"Expect refined keypoints shape is \
+                {self.cfg.num_keypoints}x2, \
+                but got {refined_pts.shape}")
+            return None
+            # raise ValueError(
+            #     f"Expect get a result of refined keypoints \
+            #         with shape {self.cfg.num_keypoints}x2, \
+            #         but got {refined_pts.shape}")
+        
         packet.refined_pts2d = refined_pts
         packet.timing['refine'] = (time.time() - t0) * 1000.0
-        print(f"refine time: {packet.timing['refine']:.2f}ms")
+        self.logger.info(
+            f"refine time: {packet.timing['refine']:.2f}ms")
         return packet
     
-    def validate_packet(self, packet):
+    def _validate_packet(self, packet:FramePacket):
+        kp = packet.keypoints
         if packet.image is None:
-            self.logger.warning(
-                "Packet has no image for refinement")
-            return False
-        if packet.keypoints is None:
-            self.logger.warning(
-                "Packet has no keypoints for refinement")
-            return False
-        return True
+            raise ValueError("packet.image is None")
+        if not isinstance(kp, np.ndarray):
+            raise ValueError(
+                "packet.keypoints is not numpy array")
+        if kp.shape != (self.cfg.num_keypoints,2):
+            raise ValueError(f"expect keypoints shape is\
+                             {self.cfg.num_keypoints}x2,\
+                             but got {kp.shape}")
 
     def run(self):
-        mode = self.cfg.get('mode', 'offline')
-        while not self.stop_event.is_set():
-            # Get packet from input queue
-            try:
-                packet = self.in_q.get(timeout=0.1)
-            except queue.Empty:
-                continue
-            try:
-                # Handle EOF
-                if packet is None:
-                    self.logger.info(
-                        "RefineThread received EOF")
-                    self.out_q.put(None)
-                    break
-                # Process and add refined points to packet
-                packet = self.process(packet)
-
-                # Handle failed refinement
-                if packet is None:
-                    self.logger.warning(
-                        "Refinement failed for packet, skipping")
+        try:
+            while not self.stop_event.is_set():
+                # Get packet from input queue
+                try:
+                    packet = self.in_q.get(timeout=0.1)
+                except queue.Empty:
                     continue
-                # offline
-                if mode == 'offline':
-                    self.out_q.put(packet, block=True)
-                # online
-                else:
-                    # if out_q is full, drop olddest packet
-                    try:
-                        self.out_q.put_nowait(packet)
-                    except queue.Full:
-                        try:
-                            dropped = self.out_q.get_nowait()
-                            self.out_q.task_done()
-                            self.logger.warning(
-                                "Output queue full, dropping olddest packet")
-                        except queue.Empty:
-                            pass
+                try:
+                    # Handle abnormal upstream packet
+                    if packet is None:
+                        self.logger.warning(
+                            "received None packet, skipping")
+                        continue
+                    
+                    # EOF packet from upstream
+                    if getattr(packet, "eof", False):
+                        self.logger.info("received EOF packet")
+                        self.out_q.put(packet)  # pass EOF packet downstream
+                        break
 
-                        try:
-                            self.out_q.put_nowait(packet)
-                        except queue.Full:
-                            self.logger.warning(
-                                "Output queue still full")
-                
+                    # Process and add refined points to packet
+                    packet = self.process(packet)
+                    
+                    # Handle failed refinement
+                    if packet is None:
+                        self.logger.warning(
+                            "Refinement failed for packet, skipping")
+                        continue
 
-            except Exception:
-                self.logger.exception(
-                    "RefineThread error")
-            finally:
-                self.in_q.task_done()
+                    if self.cfg.queue_config.drop_oldest:
+                        put_latest(self.out_q, packet)
+                    else:
+                        self.out_q.put(packet, block=True)                    
 
-# TODO: check keypoints dims for all threads
+                except Exception:
+                    raise
+                finally:
+                    self.in_q.task_done()
+        except Exception:
+            self.logger.exception(
+                "RefineThread error")
+        finally:
+            self.executor.shutdown()
+            self.logger.info("RefineThread exited cleanly")
