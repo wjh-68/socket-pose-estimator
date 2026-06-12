ff9a9540d849a510349002879213a67b9ca2f1c2
wjh-68 <2450525294@qq.com>
2026-06-12 16:41:23 +0800

    一些参数和日志调整

--- core/queues.py
+++ core/queues.py
@@ -29,8 +29,8 @@ def put_latest(
         try:
             dropped = q.get_nowait()
             q.task_done()
-            # logger.debug(
-            #     "Output queue full, dropping olddest packet")
+            logger.debug(
+                "Output queue full, dropping olddest packet")
         except queue.Empty:
             pass
 
